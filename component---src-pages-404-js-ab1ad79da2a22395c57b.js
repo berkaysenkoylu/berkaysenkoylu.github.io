3d499b51853908400c7e649c63732a45d4b1a6b9
Berkay <senkoylu_berkay@yahoo.com>
2022-01-05 13:22:39 +0300

    Update

---
+++ component---src-pages-404-js-ab1ad79da2a22395c57b.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkportfolio=self.webpackChunkportfolio||[]).push([[883],{9616:function(e,t,n){n.r(t);var l=n(7294),u=n(363),o=n(3751);t.default=function(){return l.createElement(u.Z,null,l.createElement(o.Z,{title:"404: Not found"}),l.createElement("h1",null,"NOT FOUND"),l.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-ab1ad79da2a22395c57b.js.map
